cc8915377028a22744657e704908f0bffa0b49a6
league <mclaren.grant12@gmail.com>
2023-11-16 16:57:52 -0800

    Finished Visual Array List

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,51 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+		ArrayList<String> arr = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		arr.add("Hello");
+		arr.add("My name is Joshua.");
+		arr.add("I like to code.");
+		arr.add("Brawl Stars is fun!");
+		arr.add("Life is good.");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < 5; i++) {
+			String s = arr.get(i);
+			System.out.println(s);
+		}
+		for (String s : arr) {
+			System.out.println(s);
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < 5; i+=2) {
+			String s = arr.get(i);
+			System.out.println(s);
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for (int i = 4; i >= 0; i--) {
+			String s = arr.get(i);
+			System.out.println(s);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < 5; i++) {
+			boolean good = false;
+			String s = arr.get(i);
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == 'e') {
+					good = true;
+					break;
+				}
+			}
+			if (good) {
+				System.out.println(s);
+			}
+		}
 	}
 }
--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,28 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("7degrees");
+		list.add("destroyer");
+		list.add("JZ4444");
+		list.add("pogger69");
 		// 2. Set one item in our list to be something different
-		
+		list.set(1, "pogger");
 		// 3. Remove something from our list
-		
+		list.remove(1);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if (list.contains("7degrees")) {
+			System.out.println("7degrees is in the list");
+		}
+		System.out.println(list.get(2));
 		// 6. Get a specific item from our list and print it out
 		
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
